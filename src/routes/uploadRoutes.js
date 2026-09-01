9066ff39ff44aefbe4b81e70273e131608e7cc20
Vivek Chaurasiya <chaurasiyavivek657@gmail.com>
2026-09-01 22:42:13 +0530

    done

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -21,11 +21,11 @@ const storage = multer.diskStorage({
 });
 
 function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png|webp|gif/;
+  const filetypes = /jpg|jpeg|png|webp|gif/i;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
 
-  if (extname && mimetype) {
+  if (extname || mimetype) {
     return cb(null, true);
   } else {
     cb(new Error('Images only!'));
